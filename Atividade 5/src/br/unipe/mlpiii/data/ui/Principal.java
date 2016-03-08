bb6a2576d2f7a14afb58bc25638380f800bc44a4
arthurxii <arthur_drums@hotmail.com>
2016-03-08 09:50:30 -0300

    Added files via upload
    Questão 1

---
+++ Atividade 5/src/br/unipe/mlpiii/data/ui/Principal.java
@@ -0,0 +1,18 @@
+package br.unipe.mlpiii.data.ui;
+
+import br.unipe.mlpiii.data.modelo.Data;
+
+public class Principal {
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.setDia(04);
+		d.setMes(01); 
+		d.setAno(1995); 
+		System.out.println("Data do meu aniversario: "+d); 
+
+	}
+
+}
+
+
+       
